ccf8818d643541993d5c116c886ee4c3c876c899
Carlos Ferreira <carlos@especializati.com.br>
2023-12-13 10:22:47 -0300

    feat: list user paginate

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('/', fn () => response()->json(['message' => 'ok']));
